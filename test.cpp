d0f9ed8600a271da06f522e886eee340d66d7819
Ramilbey <ramilbey505711503@gmail.com>
2025-04-15 15:16:49 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <cmath> // Needed for pow()
+#include <cmath> 
 using namespace std;
 
 void swap(int &f, int &h) {
@@ -9,8 +9,8 @@ void swap(int &f, int &h) {
 }
 
 double volume(int r) {
-    double pi = 3.141; // or use M_PI from <cmath> for more accuracy
-    double result = pow(r, 3); // r^3
+    double pi = 3.141; 
+    double result = pow(r, 3); 
     double v = (4.0 / 3.0) * pi * result;
     return v;
 }
