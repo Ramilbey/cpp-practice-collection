c74e086188c1440666d480b431a70ee327638ef1
Ramilbey <ramilbey505711503@gmail.com>
2025-04-15 17:41:13 +0800

    clean code
    add comments, clean code(arrangemenst) shortcuts

--- seqSearch.cpp
+++ seqSearch.cpp
@@ -4,12 +4,13 @@ using namespace std;
 int seqSearch(int arr[], int n, int item){
     for(int i = 0 ; i < n; i ++)
         if(arr[i] == item)
-            cout<< arr[i];
+            return i; // returns the found item and terminates ( stops the loop)
     return -1;
 }
 int main (){ 
     int n = 20;
     int arr[n] = {1,2,3,4,5,6,7,8,9,10};
-    int item =10;
-    seqSearch(arr,n, item);
+    int item =10;//worst case
+    int index = seqSearch(arr,n, item);
+    cout <<index; //we can cout here no need in function cout
 }
