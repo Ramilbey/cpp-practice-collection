a5db83a1a36c49c0ec911f0e2e9e7a5bf8e141f5
Ramilbey <ramilbey505711503@gmail.com>
2025-04-20 18:53:33 +0800

    isAcending
    3.The code should be able to check if the array in ascending order or not.

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -1,10 +1,18 @@
+//binary search
 #include <iostream>
 using namespace std; 
-
 int main(){
   int size = 8;
-  int arr[size] = {8,4,5,6,2,1,3,7}
+  int arr[size] = {8,4,5,6,2,1,3,7};
   for(int i = 0; i< size; i++)
     cout<< arr[i] << " " ;
   cout << endl;
+  bool isAcending = false;
+  for(int i = 0; i < size -1; i++)//lcheck if it is acending
+    if(arr[i] > arr[i+1])
+      isAcending = true;
+  if(isAcending)
+    cout<< "True";
+  else
+    cout << "False";
 }
