65e0a22f139bdd9beb2c9b0fe4c0cf662fd6b185
Ramilbey <ramilbey505711503@gmail.com>
2025-05-11 22:10:47 +0800

    create func and declare variable

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -1,6 +1,15 @@
 #include <iostream> ;
 using napespace std;
 
+void mergeSort(int arr[], int first, int mid, int last){
+  int tempArray[];
+  int first1 = first; 
+  int last1 = mid;
+  int first2 = mid+1;
+  int last2 = last;
+  int index = first1;
+}
+
 int main{
   
 }
