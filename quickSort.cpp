6a1abda6b4bf05fc6e1d15a61b33235e9dbac66f
Ramilbey <ramilbey505711503@gmail.com>
2025-05-12 22:02:24 +0800

    main funct

--- quickSort.cpp
+++ quickSort.cpp
@@ -2,4 +2,13 @@
 //elements smaller than pivot go to left
 // elements bigger than pivot go to right
 //recorsively sort left and right parts
+#inlcude <iostream> 
+using namespace std;
+//Patrition array
 
+
+
+
+int main (){
+  
+}
