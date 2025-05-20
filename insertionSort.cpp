190d201d884f852fc83f504eff98e7c42ab21836
Ramilbey <ramilbey505711503@gmail.com>
2025-05-20 17:30:37 +0800

    comment usage of &&  and else break

--- insertionSort.cpp
+++ insertionSort.cpp
@@ -5,9 +5,19 @@ void insertionSort(int arr[], int n){
     for(int i = 1; i< n; i ++){
         int temp = arr[i];
         int j; 
-        for (j = i; j> 0 && temp < arr[j-1]; j--)
+        for (j = i; j> 0 && temp < arr[j-1]; j--) // use && so that loop automaticly stops
             arr[j] = arr[j-1];
         arr[j] = temp;
+        //  for(int i = 0; i <n-1; i ++){ another way doing instead of &&
+        // int temp = arr[i];
+        // int j;
+        // for(j = i; j > 0 ; j--){
+        //     if(temp < arr[j -1])
+        //         arr[j] = arr[j-1];
+        //     else
+        //         break;
+        // }
+        // arr[j] = temp; 
     }
 }
 
