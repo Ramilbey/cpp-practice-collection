384f62db782648c65c66b35b137a0a1fbb0d0172
Ramilbey <ramilbey505711503@gmail.com>
2025-05-26 20:35:41 +0800

    Create task1.cpp

---
+++ task1.cpp
@@ -0,0 +1,4 @@
+// 1.Assume you have 10 students' IDs consisting of integer numbers. Apply the most suitable searching algorithms in terms of efficiency. Depending on the content of the array, choose either Sequential Search or Binary Search (recursive) algorithms:
+// Generate a set of integers (user input) into the array in any order. All integers in the array must be unique. Ask the user to input an integer to be searched in the array.
+// Implement both Sequential Search and Binary Search (recursive) and let the program choose the most suitable algorithm to be applied.
+// Print the output if the integer exists and specify the location; otherwise, show that the integer does not exist.
