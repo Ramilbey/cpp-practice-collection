b258e5afd61ef6e14d7af7b113cb320f6727f1f9
Ramilbey <ramilbey505711503@gmail.com>
2025-05-26 23:10:15 +0800

    debug

--- task1.cpp
+++ task1.cpp
@@ -2,47 +2,48 @@
 // Generate a set of integers (user input) into the array in any order. All integers in the array must be unique. Ask the user to input an integer to be searched in the array.
 // Implement both Sequential Search and Binary Search (recursive) and let the program choose the most suitable algorithm to be applied.
 // Print the output if the integer exists and specify the location; otherwise, show that the integer does not exist.
-#include <iostream>;
+#include <iostream>
 using namespace std;
 
-void inser(int n, int arr[]){
-  cout<< "Enter the number of students";
+void insert(int n, int arr[]){
+  cout<< "Enter the number of students ";
   cin>> n;
-  cout<< "Enter students' IDS"<<endl;
+  cout<< "Enter students' IDS "<<endl;
   for( int i = 0; i < n; i++)
     cin >> arr[i];
 }
-void seqSearch(int n, int arr[]), int target{
-  // for(int i=0; i < n; i++)
-  if(arr[i] === target)
-    return seqSearch[n, arr, arr[mid];
-  return -1;
+int seqSearch(int n, int arr[], int i, int target){
+  if(i >= target)
+    return -1;
+  if(arr[i] == target)
+    return i;
+   return seqSearch(n , arr,i+1, target);
 }
-void binarySearch(int arr[], int h, int l, int target){
-  int mid (l+h) /2;
-  if(arr[mid] === target)
-    return arr[mid];
+int binarySearch(int arr[], int h, int l, int target){
+  int mid =(l+h)/2;
+  if(arr[mid] == target)
+    return mid;
   if( arr[mid] < target)
-    return binarySearch(arr, l, mid + 1, target)
+    return binarySearch(arr, mid + 1, l, target);
   if(arr[mid] > target)
-    return binarySearch(arr, mid -1, h, target)
+    return binarySearch(arr, l, mid -1, target);
   return -1;
 }
 bool isAscending(int n, int arr[]){
-  for(int i=0; i < n; i++)
-    if(arr[i]<arr[i+1]
-      return true;
-  return false;
+  for(int i=0; i < n -1; i++)
+    if(arr[i]>arr[i+1])
+      return false;
+  return true;
 }
 int main(){
   int n = 100;
   int arr[n];
   insert(n, arr);
   int target; 
-  cout<< "Enter the target";
+  cout<< "Enter the target ";
   cin >> target;
-  if(isAscending)
+  if(isAscending(n, arr))
     cout << binarySearch(arr, 0, n, target);
   else 
-    cout<< seqSearch(n, arr, target); 
+    cout<< seqSearch(n, arr, 0, target); 
 }
