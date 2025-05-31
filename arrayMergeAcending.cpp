87b37cc64fd0345d794d4c47392ced1848a453ee
Ramilbey <ramilbey505711503@gmail.com>
2025-05-31 19:34:54 +0800

    comment the task

--- arrayMergeAcending.cpp
+++ arrayMergeAcending.cpp
@@ -1,1 +1,4 @@
-
+// Write a program to merge three (3) arrays (arr1, arr2 and arr3) into another array (arrayMerg).
+// Note that, all arr1, arr2 and arr3 should be inserted in Ascending order.
+// The merged array should also be in Ascending order.
+// Note that, this task can be done without sorting.
