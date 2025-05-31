59ca81615d775cf7b0ebff77336f601c363054ae
Ramilbey <ramilbey505711503@gmail.com>
2025-05-31 21:03:19 +0800

    create loop

--- arrayMergeAcending.cpp
+++ arrayMergeAcending.cpp
@@ -5,9 +5,27 @@
 #include <iostream>
 using namespace std;
 
-void arrayMergAcednig(int arr1[], int arr2[], int arr3[], int size1, int size2, size3, int merged[]){
-  
+void arrayMergAcending(int arr1[], int arr2[], int arr3[], int size1, int size2, int size3, int merged[]){
+  for(int i = 0; i < size1; i++)
+    merged[i] = arr1[i];
+  for( int i = 0;i < size2; i++)
+    merged[i] = arr2[i];
+  for (int i=0; i< size3; i++)
+    merged[i] = arr3[i];
 }
+void loop(int merged[], int size1, int size2, int size3){
+  for(int i = 0; i< size1+ size2 + size3; i++)
+    cout<< merged[i];
+}
+void loop(
 int main(){
-  
+  int size1 = 3;
+  int arr1[size1] = {1,2,3};
+  int size2 = 3;
+  int arr2 = {4,5,6};
+  int size3 = 3;
+  int arr3[size3] ={7,8,9};
+  int mergedArray[size1 + size2 + size3];
+  arrayMergAcending(arr1, arr2, arr3, size1,size2, size3, mergedArray);
+  loop(mergedArray, size1, size2, size3);
 }
