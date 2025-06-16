ea0283e3f296c27c1890fde7222ba8b63d5018a2
Ramilbey <ramilbey505711503@gmail.com>
2025-06-16 22:38:09 +0800

    commnet the task

--- checkFrequency.cpp
+++ checkFrequency.cpp
@@ -1,3 +1,10 @@
+// 4.Write a C++ program to apply a recursive function to calculate the frequencies of all numbers in a one-dimensional array. Generate random numbers between 0 and 30 into an array size of 30. Provide the complexity of your code in terms of big Oh notation. 
+// Steps:
+// a.Assign random numbers to the array
+// b.Calculate the frequency of all numbers in the array
+// c.Store the frequencies of all numbers
+// d.Sort the frequencies from smallest to largest.
+// e.Display every number and its frequency.
 #include <iostream>
 #include <cstdlib>
 using namespace std;
