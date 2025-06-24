7678f9bdc00f0d0a21eae7b4eb8274e6e8dcf066
Ramilbey <ramilbey505711503@gmail.com>
2025-06-24 18:09:27 +0800

    insert front , middle , end

--- linkedList.cpp
+++ linkedList.cpp
@@ -23,20 +23,51 @@ int findNode(Node *&head, int value){
     Node *curr=head;
     int currentIndex = 1;
     while( curr){
-        if( curr->data ==value)
+        if( curr->data == value)
             return currentIndex;
         curr=curr->next;
         currentIndex++;
     }
     return -1;
 }
+void insertEnd(Node *head, int value){
+    Node *curr=head;
+    while(curr-> next)
+        curr=curr->next;
+    Node *newNode=createNode(value);
+    curr-> next =newNode;
+}
+void insertFront(Node*& head){
+    
+}
+void insertMidlle(Node* head, int value, int index){
+    Node* curr =head;
+    int currentIndex = 1;
+    while(curr){
+        currentIndex++;
+        if( currentIndex == index){
+            currentIndex +=1;
+            Node* newNode = creatNode(value);
+            newNode = currentIndex;
+        }
+    }
+}
 
 int main() {
     Node *head=createNode(10);
     head->next = createNode(20);
     head->next->next = createNode(30);
+    while(curr->next)
+        
     printList( head);
     cout<< findNode(head, 30);
-
+    
+    Node 
+    
+    
     return 0;
 }
+
+
+
+
